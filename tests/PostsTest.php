69576b72438cebe3517c367f2347dc427fe24646
Boris Jankovic <borisjanko@outlook.com>
2024-06-27 09:39:28 +0200

    Finish PostsTest

--- tests/PostsTest.php
+++ tests/PostsTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 use function PHPUnit\Framework\assertTrue;
 
 require_once __DIR__ . '/../models/Database.php';
-require_once __DIR__ . '/../models/Post.php';
+require_once __DIR__ . '/../models/Posts.php';
 
 class PostsTest extends TestCase {
 
@@ -13,15 +13,60 @@ class PostsTest extends TestCase {
         $feedId = 1;
         $testPosts = [
             [
-            'id' => 1,
-            'feed_id' => $feedId,
-            'url' => 'http://example.com/post21'
+                'id' => 1,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post21'
+            ],
+            [
+                'id' => 2,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post22'
+            ],
+            [
+                'id' => 3,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post23'
+            ],
+            [
+                'id' => 4,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post24'
+            ],
+            [
+                'id' => 5,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post25'
+            ],
+            [
+                'id' => 6,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post26'
+            ],
+            [
+                'id' => 7,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post27'
+            ],
+            [
+                'id' => 8,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post28'
+            ],
+            [
+                'id' => 9,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post29'
+            ],
+            [
+                'id' => 10,
+                'feed_id' => $feedId,
+                'url' => 'http://example.com/post30'
             ]
         ];
 
         $db = new Database();
         $post = new Post($db);
-        $postData = $post->getPostsByFeedId($feedId, 5);
+        $postData = $post->getPostsByFeedId($feedId, 10);
 
         $this->assertIsArray($postData);
         $this->assertSame($testPosts, $postData);
